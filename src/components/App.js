2226916bdc9bd66768a66545d219fd1c8387cb9d
Jonathan Canfield <jonathantcanfield@gmail.com>
2017-09-15 11:00:19 -0400

    add apit

--- src/components/App.js
+++ src/components/App.js
@@ -7,14 +7,24 @@ class App extends Component {
   // You should set state for vehicles (empty array), value (empty string), pilot (empty) string.
   // Enter your code below:
 
-
+  constructor() {
+    super();
+    this.handleNameChange = this.handleNameChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.state = {
+      vehicles: [],
+      value: '',
+      pilot: ''
+    };
+  }
 
   // FORM: HANDLE INPUT CHANGES
   // handleNameChange below:
   // See form lesson for details.
   // Enter your code below:
-
-
+  handleNameChange(event) {
+    this.setState({value: event.target.value});
+  }
 
   //  FORM: SUBMIT METHOD
   // handleSubmit below:
@@ -23,6 +33,15 @@ class App extends Component {
   // Then, set the value of the input back to an empty string.
   // Enter your code below:
 
+  handleSubmit(event) {
+    event.preventDefault();
+    this.setState(
+      {
+        pilot: this.state.value,
+        value: ''
+      }
+    )
+  }
 
   // LIFECYCLE
   // Which lifecycle is best for fetching data?
@@ -32,6 +51,14 @@ class App extends Component {
   // You will want to use this array when you set the state of 'vehicles'. You will need this data in your render.
   // Enter your code below:
 
+  componentDidMount() {
+    fetch('https://swapi.co/api/vehicles/').then((response) => {
+      return response.json()
+    }).then((data) => {
+      let vehicles = data.results;
+      this.setState({vehicles: vehicles})
+    })
+  }
 
   // RENDER
   // Before you can map over the data you've fetched, you will first need to store that 'state' in a variable.
@@ -46,14 +73,58 @@ class App extends Component {
     Store vehicles state in a variable.
     Map over this variable to access the values needed to render.
     */
+    let vehicleArray = this.state.vehicles;
+    let vehicles = vehicleArray.map((vehicles) => {
+      return (
+      /* Create the vehicle card below: */
+      < div key = {vehicles.name} className = "col-md-4" >
+        <div className="card">
+          <div className="card-block">
+            <h4 className="card-title">Vehicle: {vehicles.name}</h4>
+            <h5 className="card-subtitle mb-2 text-muted">Model: {vehicles.model}</h5>
+            <div className="card">
+              <div className="card-block">
+                <h5 className="card-subtitle mb-2 text-muted">Specs</h5>
+                <ul className="list-group list-group-flush">
+                  <li className="list-group-item">Manufacturer: {vehicles.manufacturer}</li>
+                  <li className="list-group-item">Class: {vehicles.vehicle_class}</li>
+                  <li className="list-group-item">Passengers: {vehicles.passengers}</li>
+                  <li className="list-group-item">Crew: {vehicles.crew}</li>
+                  <li className="list-group-item">Length: {vehicles.length}</li>
+                  <li className="list-group-item">Max Speed: {vehicles.max_atmosphering_speed}</li>
+                  <li className="list-group-item">Cargo Capacity: {vehicles.cargo_capacity}</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      < /div>
+      )
     })
     return (
       <div className="App">
-        {/*
-        The App component needs the following:
-         jumbotron section, form section, vehicle cards section.
-         Your form will also need a header in which you will pass the state of the form upon submit.
-         */}
+        <main className="row">
+          <section className="col-md-10 offset-md-1">
+            <div className="jumbotron">
+              <h1 className="display-3">Star Wars</h1 > <hr className="my-4"/> < p className = "lead" > The Vehicles of Star Wars < /p>
+            </div >
+            <div className="card form">
+              <div className="card-block">
+                <h2 className="card-title">What is your name, pilot?</h2>
+                <form onSubmit={this.handleSubmit}>
+                  <div className="form-group">
+                    <input className="form-control col-md-4 offset-md-4" id="pilotName" onChange={this.handleNameChange} name="name" type="text" value={this.state.value} placeholder="Enter your name"/>
+                  </div>
+                  <button type="submit" className="btn btn-primary">Submit</button>
+                </form>
+                <h1>{this.state.pilot}</h1>
+              </div>
+            </div>
+            < div className = "row" >
+              {vehicles}
+            < /div>
+          </section >
+        </main>
       </div>
     );
   }
